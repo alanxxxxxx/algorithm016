5766eed8281c5ee0ca44250cec7f457493c32bf6
yalei.wang <wangyalei@inke.cn>
2020-09-27 23:58:58 +0800

    完善回溯

--- Week_03/permutations.go
+++ Week_03/permutations.go
@@ -12,6 +12,8 @@ func main() {
 	}
 	res := permute(nums)
 	fmt.Println(res)
+	res = per01(nums)
+	fmt.Println(res)
 }
 func permute(nums []int) (res [][]int) {
 	if len(nums) == 1 {
@@ -28,5 +30,38 @@ func permute(nums []int) (res [][]int) {
 			res = append(res, append(s, num))
 		}
 	}
+	return
+}
+func per01(nums []int) (res [][]int) {
+	var used = make([]bool, len(nums))
+	var tmpList = []int{}
+	var backtrace func([]int, []int, []bool)
+	backtrace = func(nums []int, tmpList []int, used []bool) {
+		// 匹配条件
+		if len(tmpList) == len(nums) {
+			c := make([]int, len(nums))
+			copy(c, tmpList)
+			res = append(res, c)
+			return
+		}
+		for i := 0; i < len(nums); i++ {
+			if used[i] {
+				continue
+			}
+			// 选择01
+			used[i] = true
+			tmpList = append(tmpList, nums[i])
+			// backtrace
+			backtrace(nums, tmpList, used)
+			// 选择02
+			// 还原操作
+			tmpList = tmpList[:len(tmpList)-1]
+			used[i] = false
+		}
+		// 返回结果
+		return
+
+	}
+	backtrace(nums, tmpList, used)
 	return
 }
